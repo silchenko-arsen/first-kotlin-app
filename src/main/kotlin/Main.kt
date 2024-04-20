184292050bfa5fda379a4756c42dbba1a2c90c78
silchenko-arsen <silchenkoarsen@gmail.com>
2024-04-20 16:06:58 +0300

    do exercise

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    println("Enter your city: ")
+    val city = readLine()
+    println("User lives in $city")
 }
